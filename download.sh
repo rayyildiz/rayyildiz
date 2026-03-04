ab78a68f5e239e0d97692646d7772e41e1bf2e6c
Ramazan A. <r@ayyildiz.ai>
2026-03-04 17:08:38 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,3 +1,6 @@
-curl https://stats.rayyildiz.com/api?username=rayyildiz&show_icons=true&hide_border=true > icons/github-stats.svg
 
-curl https://stats.rayyildiz.com/api/top-langs/?username=rayyildiz&langs_count=7&cache_seconds=3000&hide=Asp,Ruby,HTML,css,javascript,shell,vim%20script,TypeScript,Dockerfile,Scala,Makefile,Dart,TSQL,SCSS,Emacs%20Lisp,Objective-C,C%23&show_icons=true&hide_border=true > icons/github-top7.svg
+curl -sSL "https://stats.rayyildiz.com/api?username=rayyildiz&show_icons=true&hide_border=true" \
+  -o icons/github-stats.svg
+
+curl -sSL "https://stats.rayyildiz.com/api/top-langs/?username=rayyildiz&langs_count=7&cache_seconds=3000&hide=Asp,Ruby,HTML,css,javascript,shell,vim%20script,TypeScript,Dockerfile,Scala,Makefile,Dart,TSQL,SCSS,Emacs%20Lisp,Objective-C,C%23&show_icons=true&hide_border=true" \
+  -o icons/github-top7.svg
